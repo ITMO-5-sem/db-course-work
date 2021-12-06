1dd1f8044bd3e32bafaee01e1384a57d529c175c
Kirill Kondrashov <64485121+kirillkon1@users.noreply.github.com>
2021-12-06 15:56:58 +0300

    Update create-tables.sql
    Updated create-table script

--- db/src/create-tables.sql
+++ db/src/create-tables.sql
@@ -1,64 +1,45 @@
-create sequence object_id_sequence
-    start 1
-    increment 1;
-
-
 create table sector
 (
     id   serial primary key,
     name varchar(64) not null
-        check ( length( name ) > 0 )
+        check ( length(name) > 0 )
 );
 
 
-create table system
+create table systemSR
 (
     id     serial primary key,
     name   varchar(64) not null unique
-        check ( length( name ) > 0 ),
+        check ( length(name) > 0 ),
     sector int         not null
         references sector
             on delete cascade
             on update cascade
 );
 
-create table space_object_type(
-    id serial primary key,
-    name varchar(64) not null
-);
-
-create table space_object(
-    id serial primary key,
-    name varchar(64) not null unique check ( length( name ) > 0 ),
-    object_type int references space_object_type,
-    system int references system on delete cascade not null
-);
-
 create table spacebase_type
 (
     id          serial primary key,
-    name        varchar(16)  not null unique check ( length( name ) > 0 ),
+    name        varchar(16) not null unique check ( length(name) > 0 ),
     description varchar(512),
     karma_from  int check ( karma_from >= -100 and karma_from < 100 ),
     karma_to    int check (karma_to < 100 and karma_to > -100
-                and karma_to > karma_from)
+        and karma_to > karma_from)
 );
 
 
 create table spacebase
 (
-    id          serial primary key,
-    obj_id int references space_object on delete cascade on update cascade not null,
+    id                int primary key,
+    name              varchar(64) not null unique check ( length(name) > 0 ),
     spacebase_type_id int         not null references spacebase_type on delete restrict
-        on update cascade, -- no reason to delete the type
+        on update cascade,    -- no reason to delete the type
 
     system_id         int         not null
-        references system
-            on delete cascade  -- delete a system - delete everything
+        references systemSR
+            on delete cascade -- delete a systemSR - delete everything
             on update cascade
 );
-
-
 
 
 
@@ -66,9 +47,9 @@ create table politics
 (
     id          serial primary key,
     name        varchar(32)  not null
-        check ( length( name ) > 0 ),
+        check ( length(name) > 0 ),
     description varchar(256) not null
-        check ( length( description ) > 0 )
+        check ( length(description) > 0 )
 );
 
 
@@ -76,9 +57,9 @@ create table economics
 (
     id          serial primary key,
     name        varchar(32)  not null
-        check ( length( name ) > 0 ),
+        check ( length(name) > 0 ),
     description varchar(256) not null
-        check ( length( description ) > 0 )
+        check ( length(description) > 0 )
 );
 
 
@@ -86,16 +67,18 @@ create table race
 (
     id          serial primary key,
     name        varchar(32)  not null
-        check ( length( name ) > 0 ),
+        check ( length(name) > 0 ),
     description varchar(256) not null
-        check ( length( description ) > 0 )
+        check ( length(description) > 0 )
 );
 
 create table planet
 (
-    
-    obj_id int references space_object on delete cascade on update cascade not null,
-    citizens  int not null
+
+    id        int primary key,
+    name      varchar(64)                               not null unique check ( length(name) > 0 ),
+    systemSR  int references systemSR on delete cascade not null,
+    citizens  int                                       not null
         check ( citizens >= 0 ), -- citizens - кол-во жителей (в тыс.)
     politics  int
         references politics
@@ -111,7 +94,7 @@ create table action_type
 (
     id            serial primary key,
     name          varchar(32) not null unique
-        check ( length( name ) > 0 ),
+        check ( length(name) > 0 ),
     action_impact int         not null
         check
             (
@@ -120,39 +103,41 @@ create table action_type
             )
 );
 
-create table user_role(
-                          id serial primary key,
-                          role_name varchar(32)
+create table user_role
+(
+    id        serial primary key,
+    role_name varchar(32)
 );
 
-create table "user"(
-                       id serial primary key,
-                       login varchar(64) not null,
-                       password varchar(64) not null,
-                       role integer references user_role not null
+create table username
+(
+    id       serial primary key,
+    login    varchar(64)                  not null,
+    password varchar(64)                  not null,
+    role     integer references user_role not null
 );
 
 create table pilot
 (
-    id               serial primary key,
-    name             varchar(64) not null unique
-        check ( length( name ) > 0 ),
-    description      varchar(256)
-        check ( length( description ) > 0),
-    race_id          int
+    id          serial primary key,
+    name        varchar(64)                                not null unique
+        check ( length(name) > 0 ),
+    description varchar(256)
+        check ( length(description) > 0),
+    race_id     int
         references race
             on delete set null
             on update cascade,
-    karma            int         not null default 0,
+    rating      int                                        not null default 0,
 
-    owner integer references "user" on delete cascade not null
+    owner       integer references username on delete cascade not null
 );
 
 create table spaceship_type
 (
     id          serial primary key,
     name        varchar(16) not null unique
-        check ( length( name ) > 0 ),
+        check ( length(name) > 0 ),
     description varchar(256)
 );
 
@@ -160,12 +145,12 @@ create table spaceship_type
 create table spaceship
 (
     id                serial primary key,
-    name              varchar(64) not null unique,
-    spaceship_type_id int         not null
+    name              varchar(64)          not null unique,
+    spaceship_type_id int                  not null
         references spaceship_type
             on delete set null
             on update cascade,
-    pilot_id          int         not null
+    pilot_id          int references pilot not null
         references pilot
             on delete cascade -- spaceship without pilot doesn't exist
             on update cascade
@@ -175,7 +160,7 @@ create table spaceship
 create table action
 (
     id                 serial primary key,
-    date               date not null default now( ),
+    date               date not null default now(),
     action_description text,
     pilot_id           int  not null
         references pilot
@@ -188,7 +173,7 @@ create table action
 );
 
 
-create table LIVING_RACES
+create table living_races
 (
     id             serial primary key,
     race_id        int
@@ -196,16 +181,17 @@ create table LIVING_RACES
             on delete cascade
             on update cascade not null,
     planet_info_id int
-        references planet_info
+        references planet
             on delete cascade
             on update cascade not null,
-    unique ( race_id, planet_info_id )
+    unique (race_id, planet_info_id)
 );
 
-create table landings(
+create table landings
+(
+    landing_id   int primary key,
     spaceship_id integer references spaceship on delete cascade not null,
-    space_obj_id integer references space_object on delete cascade not null,
-    primary key (spaceship_id, space_obj_id)
+    planet_id    integer                                        references planet on delete set null,
+    spacebase_id integer                                        references spacebase on delete set null,
+    check (not (planet_id is not null and spacebase_id is not null))
 )
-
-
